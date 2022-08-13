457376981fae29b41d8244c5ebcb34edac2feaad
SaraIbrahim94 <sara.i_94@hotmail.com>
2022-08-13 13:17:14 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <header>
+    <div id="app">
+      <Header />
+      <Latest />
+      <Posts />
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+import  Header  from './components/header.vue'
+import  Latest  from './components/Latest.vue'
+import  Posts  from './components/Posts.vue'
+
+export default {
+    name: 'app',
+    components: {
+    Header,
+    Latest,
+    Posts
+}
+}
+</script>
+
+<style lang="scss">
+body{
+  background-color: #F3F3F3;
+  font-family: "Rockwell", serif;
+}
+
+h3{
+  color: #171717;
+  font-size: 28px;
+  font-weight: 700;
+}
+</style>
